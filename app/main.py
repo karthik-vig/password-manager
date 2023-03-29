4afd2d672fc391da1d44c556753c2fc666ce07d2
Karthik Vignesh Sundar <karthik.vignesh.sun@gmail.com>
2023-03-29 05:06:16 +0100

    added new frame for push and pull sync. created a window for resetting password. add new icons for buttons in push and pull sync as well as reset password button.

--- app/main.py
+++ app/main.py
@@ -4,12 +4,70 @@ from PIL import Image
 from functools import partial
 
 
+
+# the class with all the icons
+class Icons:
+    def __init__(self):
+        self.getButtonIcons()
+        self.getEntryTypeIcons()
+
+    # gets and sets the button icons
+    def getButtonIcons(self):
+        self.addImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/add.png'),
+                                    size=(20, 20)
+                                    )
+        self.searchImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/search.png'),
+                                    size=(20, 20)
+                                    )
+        self.resetImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/reset.png'),
+                                    size=(20, 20)
+                                    )
+        self.newEntryImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/newEntry.png'),
+                                        size=(20, 20)
+                                        )
+        self.deleteEntryImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/deleteEntry.png'),
+                                            size=(20, 20)
+                                            )
+        self.updateEntryImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/updateEntry.png'),
+                                           size=(20, 20)
+                                           )
+
+    # gets and sets the entry type icons
+    def getEntryTypeIcons(self):
+        entryTypeList = ['bank',
+                        'communication',
+                        'email',
+                        'entertainment',
+                        'games',
+                        'general',
+                        'government',
+                        'office',
+                        'personal',
+                        'shopping',
+                        'streaming',
+                        'study',
+                        'transport'
+                        ]
+        self.entryTypeImgDict = {}
+        for entryType in entryTypeList:
+            entryTypeImg = ctk.CTkImage(dark_image=Image.open(f"icons/entryTypeIcon/{entryType}.png"),
+                                        size=(20, 20)
+                                        )
+            self.entryTypeImgDict[entryType] = entryTypeImg
+
+
 # The main app window
 class Window(ctk.CTk):
-    def __init__(self):
+    def __init__(self, iconObj):
         super().__init__()
+        self.iconObj = iconObj
         self.title("Password Manager")
         self.drawPasswordAuth()
+
+    # draws the windows to create a new sqlite
+    # database if an existing one is not found
+    def drawCreateNewDB(self):
+        pass
 
     # draws the password input and submission window
     # upon authetication; will draw the content window
@@ -33,11 +91,11 @@ class Window(ctk.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=2)
         # place the frames in the grid
-        self.searchFrame = SearchFrame(parent=self, height=0, width=0)
+        self.searchFrame = SearchFrame(parent=self, iconObj=self.iconObj, height=0, width=0)
         self.searchFrame.grid(row=0, column=0, rowspan=1, columnspan=1, sticky='nsew', padx=5, pady=8)
-        self.ribbonFrame = RibbonFrame(parent=self, height=0, width=0)
+        self.ribbonFrame = RibbonFrame(parent=self, iconObj=self.iconObj, height=0, width=0)
         self.ribbonFrame.grid(row=0, column=1, rowspan=1, columnspan=1, sticky='nsew', padx=5, pady=8)
-        self.itemListFrame = ItemListFrame(parent=self, height=0, width=0)
+        self.itemListFrame = ItemListFrame(parent=self, iconObj=self.iconObj, height=0, width=0)
         self.itemListFrame.grid(row=1, column=0, rowspan=1, columnspan=1, sticky='nsew', padx=5, pady=8)
         self.itemInfoFrame = ItemInformationFrame(parent=self, height=0, width=0)
         self.itemInfoFrame.grid(row=1, column=1, rowspan=1, columnspan=1, sticky='nsew', padx=5, pady=8)
@@ -98,7 +156,7 @@ class PasswordAuthFrame(ctk.CTkFrame):
 
 # The frame that contains the add button, search box and the search button
 class SearchFrame(ctk.CTkFrame):
-    def __init__(self, parent, **kwargs):
+    def __init__(self, parent, iconObj, **kwargs):
         super().__init__(parent, **kwargs)
         self.parent = parent
         # add the search box
@@ -108,11 +166,13 @@ class SearchFrame(ctk.CTkFrame):
                                       height=0)
         self.searchBox.pack(side='left', fill='both', expand=True)
         # add the search button
+        '''
         searchImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/search.png'),
                                  size=(20, 20)
                                 )
+        '''
         self.searchButton = ctk.CTkButton(self,
-                                          image=searchImg,
+                                          image=iconObj.searchImg,
                                           text='',
                                           width=50,
                                           height=0,
@@ -139,14 +199,16 @@ class SearchFrame(ctk.CTkFrame):
 
 # the frame that contains additional functionality and buttons for the app
 class RibbonFrame(ctk.CTkFrame):
-    def __init__(self, parent, **kwargs):
+    def __init__(self, parent, iconObj, **kwargs):
         super().__init__(parent, **kwargs)
         # add the add new entry button
+        '''
         addImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/add.png'),
                               size=(20, 20)
                               )
+        '''
         self.addButton  = ctk.CTkButton(self,
-                                        image=addImg,
+                                        image=iconObj.addImg,
                                         text='',
                                         width=50,
                                         height=0,
@@ -154,17 +216,242 @@ class RibbonFrame(ctk.CTkFrame):
                                         fg_color='green',
                                         hover_color='#125200',
                                         command=self.addAction)
-        self.addButton.pack(side='top', fill='y', expand=True)
+        self.addButton.pack(side='left', fill='y', expand=True, padx=2, pady=5)
+        # add a reset password button
+        '''
+        resetImg = ctk.CTkImage(dark_image=Image.open('icons/buttonIcon/reset.png'),
+                                size=(20, 20)
+                                )
+        '''
+        self.resetPasswordButton = ctk.CTkButton(self,
+                                                 image=iconObj.resetImg,
+                                                 text='',
+                                                 width=50,
+                                                 height=0,
+                                                 anchor='center',
+                                                 fg_color='#585f63',
+                                                 hover_color='#353a3d',
+                                                 command=self.resetPassword)
+        self.resetPasswordButton.pack(side='left', fill='y', expand=True, padx=2, pady=5)
+        # add the frame for push sync
+        self.pushSyncFrame = PushSyncFrame(parent=self, iconObj=iconObj)
+        self.pushSyncFrame.pack(side='left', fill='y', expand=True, padx=2, pady=5, ipadx=20)
+        # add the frame for pull sync
+        self.pullSyncFrame = PullSyncFrame(parent=self, iconObj=iconObj)
+        self.pullSyncFrame.pack(side='left', fill='y', expand=True, padx=2, pady=5, ipadx=20)
 
     # Create a new top-level windows to enter new entry details
     def addAction(self):
         self.addNewEntry = AddNewEntryToplevel(parent=self)
 
+    # resets the password of the sqilte database
+    def resetPassword(self):
+        print('reset pass')
+        self.resetPassFrame = ResetPasswordFrame(parent=self)
+
+
+
+
+# push sync class, used to push the current state
+# of the internal database onto the external database
+class PushSyncFrame(ctk.CTkFrame):
+    def __init__(self, parent, iconObj, **kwargs):
+        super().__init__(parent, **kwargs)
+        # the label frame
+        self.pushSyncLable = ctk.CTkLabel(self,
+                                          text='Push-Sync: ')
+        self.pushSyncLable.pack(side='left')
+        # the new entry button
+        self.newEntryButton = ctk.CTkButton(self,
+                                            text='',
+                                            image=iconObj.newEntryImg,
+                                            anchor='center',
+                                            width=50,
+                                            height=0,
+                                            command=self.setNewEntry
+                                            )
+        self.newEntryButton.pack(side='left', fill='y', expand=True)
+        # the delte entry button
+        self.newEntryButton = ctk.CTkButton(self,
+                                            text='',
+                                            image=iconObj.deleteEntryImg,
+                                            anchor='center',
+                                            width=50,
+                                            height=0,
+                                            command=self.deleteExternalDBEntry
+                                            )
+        self.newEntryButton.pack(side='left', fill='y', expand=True)
+        # the update modifications button
+        self.newEntryButton = ctk.CTkButton(self,
+                                            text='',
+                                            image=iconObj.updateEntryImg,
+                                            anchor='center',
+                                            width=50,
+                                            height=0,
+                                            command=self.updateExternalDatabaseEntry
+                                            )
+        self.newEntryButton.pack(side='left', fill='y', expand=True)
+
+    # the action to set new entries in the internal database 
+    # onto the external database
+    def setNewEntry(self):
+        print('setting new entries in the external database')
+    
+    # the action to delete new entries in the external database 
+    # not in the internal database
+    def deleteExternalDBEntry(self):
+        print('deletting external db entry not in the internal database')
+    
+    # the action to make the rows in the external database conform
+    # according to the internal database
+    def updateExternalDatabaseEntry(self):
+        print('updating external database to conform to internal database')
+
+
+    
+# pull sync class, used to pull the current state
+# of the external database onto the internal database
+class PullSyncFrame(ctk.CTkFrame):
+    def __init__(self, parent, iconObj, **kwargs):
+        super().__init__(parent, **kwargs)
+        # the label frame
+        self.pushSyncLable = ctk.CTkLabel(self,
+                                          text='Pull-Sync: ')
+        self.pushSyncLable.pack(side='left')
+        # the new entry button
+        self.newEntryButton = ctk.CTkButton(self,
+                                            text='',
+                                            image=iconObj.newEntryImg,
+                                            anchor='center',
+                                            width=50,
+                                            height=0,
+                                            command=self.setNewEntry
+                                            )
+        self.newEntryButton.pack(side='left', fill='y', expand=True)
+        # the delte entry button
+        self.newEntryButton = ctk.CTkButton(self,
+                                            text='',
+                                            image=iconObj.deleteEntryImg,
+                                            anchor='center',
+                                            width=50,
+                                            height=0,
+                                            command=self.deleteInternalDBEntry
+                                            )
+        self.newEntryButton.pack(side='left', fill='y', expand=True)
+        # the update modifications button
+        self.newEntryButton = ctk.CTkButton(self,
+                                            text='',
+                                            image=iconObj.updateEntryImg,
+                                            anchor='center',
+                                            width=50,
+                                            height=0,
+                                            command=self.updateInternalDatabaseEntry
+                                            )
+        self.newEntryButton.pack(side='left', fill='y', expand=True)
+
+    # the action to set new entries in the external database 
+    # onto the internal database
+    def setNewEntry(self):
+        print('setting new entries in the internal database')
+    
+    # the action to delete new entries in the internal database 
+    # not in the external database
+    def deleteInternalDBEntry(self):
+        print('deletting internal db entry not in the exteranl database')
+    
+    # the action to make the rows in the internal database conform
+    # according to the external database
+    def updateInternalDatabaseEntry(self):
+        print('updating internal database to conform to external database')
+
+
+
+# Reset Password frame
+class ResetPasswordFrame(ctk.CTkToplevel):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.geometry("600x200")
+        self.resizable(False, False)
+        self.title('Set New Password')
+        self.grab_set()
+        # configure the rows and columns
+        for row in range(4):
+            self.grid_rowconfigure(row, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=3)
+        # enter current password label and entry
+        self.currentPasswordLabel = ctk.CTkLabel(self,
+                                                 text='Enter Current Password: ')
+        self.currentPasswordLabel.grid(row=0,
+                                      column=0,
+                                      rowspan=1,
+                                      columnspan=1,
+                                      sticky='w'
+                                      )
+        self.currentPasswordEntry = ctk.CTkEntry(self)
+        self.currentPasswordEntry.grid(row=0,
+                                      column=1,
+                                      rowspan=1,
+                                      columnspan=1,
+                                      sticky='we'
+                                      )
+        # enter new password label and entry
+        self.newPasswordLabel = ctk.CTkLabel(self,
+                                             text='Enter the New Password: ')
+        self.newPasswordLabel.grid(row=1,
+                                    column=0,
+                                    rowspan=1,
+                                    columnspan=1,
+                                    sticky='w'
+                                    )
+        self.newPasswordEntry = ctk.CTkEntry(self)
+        self.newPasswordEntry.grid(row=1,
+                                    column=1,
+                                    rowspan=1,
+                                    columnspan=1,
+                                    sticky='we'
+                                    )
+        # re-enter new password label and entry
+        self.reenterPasswordLabel = ctk.CTkLabel(self,
+                                                 text='Re-Enter the New Password: ')
+        self.reenterPasswordLabel.grid(row=2,
+                                      column=0,
+                                      rowspan=1,
+                                      columnspan=1,
+                                      sticky='w'
+                                      )
+        self.reenterPasswordEntry = ctk.CTkEntry(self)
+        self.reenterPasswordEntry.grid(row=2,
+                                      column=1,
+                                      rowspan=1,
+                                      columnspan=1,
+                                      sticky='we'
+                                      )
+        # the submit button
+        self.enterSubmitButton = ctk.CTkButton(self,
+                                               text='Submit',
+                                               anchor='center',
+                                               fg_color='green',
+                                               hover_color='#125200',
+                                               command=self.resetPassword
+                                               )
+        self.enterSubmitButton.grid(row=3,
+                                    column=1,
+                                    rowspan=1,
+                                    columnspan=1,
+                                    sticky='w'
+                                    )
+
+    # the action for reset the password
+    def resetPassword(self):
+        print('resetting password')
+        self.destroy()
+
 
 
 # A scrollable frame that contains the list of buttons to show the values of a given entry
 class ItemListFrame(ctk.CTkScrollableFrame):
-    def __init__(self, parent, **kwargs):
+    def __init__(self, parent, iconObj, **kwargs):
         super().__init__(parent, **kwargs)
         self.parent = parent
         self.buttonItemList = []
@@ -179,13 +466,16 @@ class ItemListFrame(ctk.CTkScrollableFrame):
     # adds a list of buttons based on the values in itemList
     def addItemList(self, itemList):
         for item in itemList:
+            '''
             img = ctk.CTkImage(dark_image=Image.open(f"icons/entryTypeIcon/{item['type']}.png"),
                                size=(30, 30)
                               )
+            '''
+            entryTypeImg = iconObj.entryTypeImgDict[item['type'].lower()]
             buttonCallback = partial(self.getEntryDetail, item['id'])
             button = ctk.CTkButton(self,
                                    text=item['text'],
-                                   image=img,
+                                   image=entryTypeImg,
                                    compound='left', 
                                    anchor='w', 
                                    fg_color="#4a4a4a", 
@@ -550,5 +840,6 @@ if __name__ == "__main__":
     ctk.set_appearance_mode("dark")
     ctk.set_widget_scaling(1.1)
     ctk.set_window_scaling(1.0)
-    app = Window()
+    iconObj = Icons()
+    app = Window(iconObj  = iconObj)
     app.mainloop()
